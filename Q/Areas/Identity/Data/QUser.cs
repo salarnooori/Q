d14bdcfc4713fe81150e7b6331222e3a691cdc87
pars <pars@DESKTOP-2780RIB>
2022-06-10 19:51:40 +0430

    Add project files.

---
+++ Q/Areas/Identity/Data/QUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Q.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the QUser class
+public class QUser : IdentityUser
+{
+    [PersonalData]
+    [Column(TypeName = "int")]
+    public int Experience { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "int")]
+    public int ThemeId { get; set; }
+}
+
